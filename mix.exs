39f91e6033cf8cac7b076194e385e4ff88f38fbd
Comamoca <comamoca.dev@gmail.com>
2025-11-10 07:19:34 +0900

    :package: (mix.exs) Add package metadata

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule Nosnos.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/Comamoca/nosnos"
     ]
   end
 
@@ -24,6 +27,21 @@ defmodule Nosnos.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:zigler, "~> 0.15.1", runtime: false}
+    ]
+  end
+
+  defp description do
+    "High-performance Nostr cryptographic operations library implementing BIP-340 Schnorr signatures using Zig NIFs"
+  end
+
+  defp package do
+    [
+      name: "nosnos",
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/Comamoca/nosnos"
+      },
+      files: ~w(lib .formatter.exs mix.exs README.md LICENSE.md)
     ]
   end
 end
